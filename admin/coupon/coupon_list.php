ff4bd80228149d666ff865b4d79216a2d6c742d2
rock's ezweb <alikerock@gmail.com>
2023-08-24 10:48:16 +0900

    coupon list add

--- admin/coupon/coupon_list.php
+++ admin/coupon/coupon_list.php
@@ -1,6 +1,26 @@
 <?php
   include_once $_SERVER['DOCUMENT_ROOT'].'/abcmall/inc/admin_check.php';
   include_once $_SERVER['DOCUMENT_ROOT'].'/abcmall/inc/header.php';
+
+  $pageNumber = $_GET['pageNumber'] ?? 1;
+  $pageCount = $_GET['pageCount'] ?? 10;
+  $statLimit = ($pageNumber-1)*$pageCount; // (1-1)*10 = 0, (2-1)*10 = 10
+  $endLimit = $statLimit + $pageCount;
+  $firstPageNumber = $_GET['firstPageNumber'] ?? 0 ;
+  
+  $sql = "SELECT * from coupons where 1=1" ; // and 컬러명=값 and 컬러명=값 and 컬러명=값 
+  $order = " order by cid desc";//최근순 정렬
+  $limit = " limit $statLimit, $endLimit";
+
+  $query = $sql.$order.$limit; //쿼리 문장 조합
+
+  // var_dump($query);
+  
+  $result = $mysqli -> query($query);
+  
+  while($rs = $result -> fetch_object()){
+    $rsc[] = $rs;
+  }
 ?>
 
 <div class="container">
@@ -18,6 +38,43 @@
         <th scope="col">상태</th>
       </tr>
     </thead>
+    <tbody>
+      <?php
+        if(isset($rsc)){
+          foreach($rsc as $item){            
+      ?>
+      <tr>
+        <td class="coupon_thumb"><img src="<?= $item -> coupon_image; ?>" alt="<?= $item -> coupon_name; ?>"></td>
+        <td><?= $item -> coupon_name; ?></td>
+        <td><?= $item -> coupon_type; ?></td>
+        <td><?= $item -> coupon_price; ?></td>
+        <td><?= $item -> coupon_ratio; ?></td>
+        <td><?= $item -> use_min_price; ?></td>
+        <td><?= $item -> max_value; ?></td>
+        <td>
+          <?php 
+          if($item -> status == 1){
+            echo '대기';
+          } else if ($item -> status == 2){
+            echo '사용중';
+          } else {
+            echo '폐기';
+          } 
+          ?>
+        </td>
+      </tr>
+      <?php
+        } //foreach
+      } else {    
+      ?>  
+      <tr>
+        <td colspan="8">조회 결과가 없습니다.</td>
+      </tr>
+      <?php
+        }  
+      ?>  
+
+    </tbody>
   </table>
   <a href="coupon_up.php" class="btn btn-primary">쿠폰 등록</a>
 
