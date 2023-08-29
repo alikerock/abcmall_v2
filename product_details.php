cd957e07b5163ad7d0b20776d49ce5157a29058f
rock's ezweb <alikerock@gmail.com>
2023-08-29 11:20:33 +0900

    최근본 상품

--- product_details.php
+++ product_details.php
@@ -31,6 +31,32 @@ $related_cate_rt = $mysqli -> query($rsql);
 while($rcr = $related_cate_rt -> fetch_object()){
     $rr[]=$rcr;
 }
+
+$i = 0; //쿠키에 상품정보를 등록할 때 사용할 인덱스
+
+if($_COOKIE['recent_view_pd']){ //recent_view_pd이름의 쿠키 존재유무
+    $pvc = json_decode($_COOKIE['recent_view_pd']);//쿠키의 json값을 배열로 변경
+    if(!in_array($rs, $pvc)){
+        if(sizeof($pvc)>=3){ //이미 3개의 쿠키가 있다면 
+            unset($pvc[0]); //배열의 첫번째 값을 지운다.
+        }
+        ksort($pvc); //연관배열의 키값을 기준으로 오름차순, abc 순으로
+
+        foreach ($pvc as $pc) {
+            $rvarr[$i] = $pc; //오름차순 정렬된 값을 새배열에 할당.
+            $i++;
+        }
+        $rvarr[$i] = $rs; //배열에 마지막에 현재상품정보를 추가
+        $ckval = json_encode($rvarr);//쿠키에 넣기전에 쿠키형식으로 encode;
+        setcookie('recent_view_pd', $ckval, time()+86400); //24시간유지되는 쿠키 생성
+    }
+} else{
+    //recent_view_pd 쿠키 생성
+    $rvarr[$i] = $rs; //배열에 마지막에 현재상품정보를 추가
+    $ckval = json_encode($rvarr);//쿠키에 넣기전에 쿠키형식으로 encode;
+    setcookie('recent_view_pd', $ckval, time()+86400); //24시간유지되는 쿠키 생성
+}
+
 ?>
 
         <!-- <<<<<<<<<<<<<<<<<<<< Breadcumb Area Start <<<<<<<<<<<<<<<<<<<< -->
