e35dc918159697d32693eebffe542b4ec4a1b379
rock's ezweb <alikerock@gmail.com>
2023-08-30 11:26:01 +0900

    front login page add

---
+++ login_ok.php
@@ -0,0 +1,31 @@
+<?php
+  session_start(); 
+  include_once $_SERVER['DOCUMENT_ROOT'].'/abcmall/admin/inc/dbcon.php';
+
+  $userid = $_POST['userid'];
+  $userpw = $_POST['passwd'];
+  $passwd = hash('sha512',$userpw); //암호를 sha512 알고리즘이용 암호화
+  
+  $query = "select * from members where userid='{$userid}' and userpw='{$passwd}'"; 
+  $result = $mysqli->query($query);
+  $rs = $result->fetch_object();
+
+  if($rs){
+
+    $_SESSION['UID'] = $rs->userid;
+    $_SESSION['UNAME'] = $rs->username;
+
+    echo "<script>
+      alert('$rs->username님 반갑습니다');
+      location.href = '/abcmall/index.php';
+    </script>";
+  } else{
+    echo "<script>
+      alert('아이디, 비번을 다시 확인하세요');
+      history.back();
+    </script>";
+  }
+
+  
+
+?>
